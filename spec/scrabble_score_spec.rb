6366f0f86e94d7f1795d8e3fd2a0f0ddda57354a
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-08-04 21:55:54 -0700

    Pass second spec.

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -5,4 +5,8 @@ describe('String#scrabble_score') do
   it('returns the correct score for a single letter') do
     expect('g'.scrabble_score()).to(eq(2))
   end
+  
+  it('returns the correct score for an entire word') do
+    expect('ghost'.scrabble_score()).to(eq(9))
+  end
 end
