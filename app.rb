036946a28871fdfef0528dadbab872b57b799228
Alexander S. Dickinson <alexandersdickinson@gmail.com>
2015-08-11 21:46:49 -0700

    First integration test.

--- app.rb
+++ app.rb
@@ -1,1 +1,17 @@
 require('sinatra')
+require('sinatra/reloader')
+require('./lib/scrabble_score')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @title = 'Scrabble Score!'
+  @header = 'Scrabble Score Calculator'
+  erb(:form)
+end
+
+get('/score') do
+  @title = 'Scrabble Score!'
+  @header = 'Your Score'
+  @word = params.fetch('word').scrabble_score()
+  erb(:score)
+end
